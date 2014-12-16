34adf4d66748d5b09339344ab287151a9e7c0147
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-12-15 22:06:55 -0500

    Generating getter and setter for mId and mTitle

--- app/src/main/java/com/detroitlabs/kyleofori/criminalintent/Crime.java
+++ app/src/main/java/com/detroitlabs/kyleofori/criminalintent/Crime.java
@@ -8,7 +8,20 @@ import java.util.UUID;
 public class Crime {
 
     private UUID mId;
+
     private String mTitle;
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
 
     public Crime() {
         mId = UUID.randomUUID();
