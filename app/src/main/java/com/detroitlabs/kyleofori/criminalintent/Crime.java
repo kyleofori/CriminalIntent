cb82319b30cf091c6e8b97e3c300c96aac90476b
Kyle Ofori <kyle.ofori@detroitlabs.com>
2014-12-17 09:22:30 -0500

    Adding fields to Crime.java, generating getters and setters, adding new widgets

--- app/src/main/java/com/detroitlabs/kyleofori/criminalintent/Crime.java
+++ app/src/main/java/com/detroitlabs/kyleofori/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.detroitlabs.kyleofori.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,8 +9,9 @@ import java.util.UUID;
 public class Crime {
 
     private UUID mId;
-
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public UUID getId() {
         return mId;
@@ -23,7 +25,24 @@ public class Crime {
         mTitle = title;
     }
 
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
     public Crime() {
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 }
