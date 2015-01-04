a017acec51a183710b063b4c1da0c3cedcfc4107
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-01-04 18:41:01 -0500

    Listing 9.14 setting up the ArrayAdapter in CrimeListFragment

--- app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimeListFragment.java
@@ -2,6 +2,7 @@ package com.detroitlabs.kyleofori.criminalintent;
 
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
 
 import java.util.ArrayList;
 
@@ -16,5 +17,10 @@ public class CrimeListFragment extends ListFragment {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(R.string.crimes_title);
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        ArrayAdapter<Crime> adapter = new ArrayAdapter<Crime>(getActivity(),
+                android.R.layout.simple_list_item_1,
+                mCrimes);
+        setListAdapter(adapter);
     }
 }
