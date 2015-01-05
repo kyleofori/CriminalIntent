84c989da3a92997fe5a6ff45635ae5e5f7f9fb2c
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-01-05 08:52:54 -0500

    Listing 11.4 set up pager adapter

--- app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimePagerActivity.java
@@ -1,21 +1,52 @@
 package com.detroitlabs.kyleofori.criminalintent;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
 import android.view.MenuItem;
+import android.view.View;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 
 public class CrimePagerActivity extends FragmentActivity {
     private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
-    
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
