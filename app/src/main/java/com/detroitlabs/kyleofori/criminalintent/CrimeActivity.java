83d657aff20d147f3feeeb3fe7ae764880f6a1f1
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-01-05 08:21:32 -0500

    Through 10.7 getting crimeID from arguments (not intent)

--- app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimeActivity.java
@@ -6,12 +6,16 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
+import java.util.UUID;
+
 
 public class CrimeActivity extends SingleFragmentActivity {
 
     @Override
     protected android.support.v4.app.Fragment createFragment() {
-        return new CrimeFragment();
+       UUID crimeId = (UUID) getIntent().getSerializableExtra(CrimeFragment.EXTRA_CRIME_ID);
+
+        return CrimeFragment.newInstance(crimeId);
     }
 
     @Override
