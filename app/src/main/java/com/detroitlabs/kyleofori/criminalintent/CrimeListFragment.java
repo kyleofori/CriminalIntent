2fc3e151020a192f215ad3a00074dba16f5c2088
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-01-04 19:10:16 -0500

    Overriding getView() listing 9.19

--- app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/detroitlabs/kyleofori/criminalintent/CrimeListFragment.java
@@ -4,8 +4,11 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -38,6 +41,24 @@ public class CrimeListFragment extends ListFragment {
 
         public CrimeAdapter(ArrayList<Crime> crimes) {
             super(getActivity(), 0, crimes);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_crime, null);
+            }
+
+            Crime c = getItem(position);
+
+            TextView titleTextView = (TextView) convertView.findViewById(R.id.crime_list_item_titleTextView);
+            titleTextView.setText(c.getTitle());
+            TextView dateTextView = (TextView) convertView.findViewById(R.id.crime_list_item_dateTextView);
+            dateTextView.setText(c.getDate().toString());
+            CheckBox solvedCheckBox = (CheckBox) convertView.findViewById(R.id.crime_list_item_solvedCheckBox);
+            solvedCheckBox.setChecked(c.isSolved());
+
+            return convertView;
         }
     }
 }
